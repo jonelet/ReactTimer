57f1072ddea6ce6bd787409150dbc4a6a4cec9aa
jonelet <jonelet@gmail.com>
2016-10-23 22:46:35 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    'script!jquery/dist/jquery.min.js',
+    'script!foundation-sites/dist/foundation.min.js',
+    './app/app.js'
+  ],
+  externals: {
+    jquery: 'jQuery'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      '$': 'jquery',
+      'jQuery' : 'jquery'
+    })
+  ],
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  resolve: {
+    root: __dirname,
+    alias: {
+      Main: 'app/components/Main.js',
+      Nav: 'app/components/Nav.js',
+      applicationStyles: 'app/styles/app.scss'
+    },
+    extenstions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0']
+        },
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/
+      }
+    ]
+  },
+  devtool: 'inline-source-map'
+};
